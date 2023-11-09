2f4c917684c3f828b71d89297d4cb890955db4ad
“Jenna <jennajonesshoot@gmail.com>
2023-11-09 11:58:40 -0700

    updates index.js with new app.listen

---
+++ All Project Code/src/index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const app = express();
+const pgp = require("pg-promise")();
+const bodyParser = require("body-parser");
+const session = require("express-session");
+
+// db config
+const dbConfig = {
+  host: "db",
+  port: 5432,
+  database: process.env.POSTGRES_DB,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+};
+
+const db = pgp(dbConfig);
+
+// db test
+db.connect()
+  .then((obj) => {
+    // Can check the server version here (pg-promise v10.1.0+):
+    console.log("Database connection successful");
+    obj.done(); // success, release the connection;
+  })
+  .catch((error) => {
+    console.log("ERROR:", error.message || error);
+  });
+
+// set the view engine to ejs
+app.set("view engine", "ejs");
+app.use(bodyParser.json());
+
+// set session
+app.use(
+  session({
+    secret: "XASDASDA",
+    saveUninitialized: true,
+    resave: true,
+  })
+);
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.get('/welcome', (req, res) => {
+    res.json({status: 'success', message: 'Welcome!'});
+});
+
+module.exports = app.listen(3000);
+console.log("Server is listening on port 3000");
